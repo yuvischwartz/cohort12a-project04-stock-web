f41097b9a8d12e71d9573fa719ea5678becc4fe1
yuvischwartz <yuval.orderdesk@gmail.com>
2021-10-23 18:08:24 -0400

    I added google fonts and edited the app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className={'grid grid-cols-12'}>
             <div className={'col-span-12 border p-3'}>
-              <h1 className={'text-3xl text-center font-bold'}>Paper Trader</h1>
+              <h1 className={'main-title text-center font-bold'}>Paper Trader</h1>
             </div>
         
             <div className={'col-span-12 md:col-span-6 border p-5'}>
@@ -17,21 +17,23 @@ function App() {
                 <div className={'col-span-12 border p-3'}>Box3</div>
                <div className={'col-span-12 md:col-span-6 border p-3'}>
                  {/* Box 4 */}
-                <h1 className={'text-2xl text-center'}>BHP - BHP Group Limited</h1>
+                <h1 className={'sub-title text-2xl text-center'}>BHP - BHP Group Limited</h1>
               </div>
             <div className={'col-span-12 md:col-span-6 border p-3'}>
               {/* Box 5 */}
-                <div className={'grid grid-cols-12'}>
-                <div className={'col-span-12 md:col-span-6 border p-3'}>
-                  <button>Buy</button></div>
-                <div className={'col-span-12 md:col-span-6 border p-3'}>
-                  <button>Sell</button></div>
+          
+              <div className={'col-span-12 md:col-span-6 flex justify-center'}>
+                  
+                    <button className={'btn bg-blue-600 hover:bg-blue-500 m-2 rounded '}>Buy</button>
+                  
+                    <button className={'btn bg-red-600 hover:bg-red-500 m-2 rounded'}>Sell</button>
                 </div>
               </div>
                 <div className={'col-span-12 border p-3'}>Box 6</div>
               </div>
             </div>
         
+    
 
             <div className={'col-span-12 md:col-span-6 border p-5'}>
             <h1 className={'text-2xl text-center font-bold'}>Box 2</h1>
