708a1fcd8c087a2ef2e378bc3605e806747bba7b
yuvischwartz <yuval.orderdesk@gmail.com>
2021-10-23 20:31:47 -0400

    Preliminary design

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ function App() {
     <>
       <div className={'grid grid-cols-12'}>
             <div className={'col-span-12 border p-3'}>
-              <h1 className={'main-title text-center font-bold'}>Paper Trader</h1>
-            </div>
+          <h1 className={'main-title text-center font-bold'}>Paper Trader</h1>
+          <h1 className={'p-text text-right pr-20'}>Welcome, Username</h1>
+        </div>
         
-            <div className={'col-span-12 md:col-span-6 border p-5'}>
-              <h1 className={'text-2xl text-center font-bold'}>Box 1</h1>
+            {/* <div className={'col-span-12 border p-3'}>
+              <h1 className={'main-title text-center font-bold'}>Paper Trader</h1>
+        </div> */}
+        
+        <div className={'col-span-12 md:col-span-6 border p-5'}>
+         {/* Box 1 */}
+              <h1 className={'text-2xl text-center font-bold'}></h1>
           
 
               <div className={'grid grid-cols-12'}>
-                <div className={'col-span-12 border p-3'}>Box3</div>
+                <div className={'col-span-12 border p-3'}>SEARCH</div>
                <div className={'col-span-12 md:col-span-6 border p-3'}>
                  {/* Box 4 */}
-                <h1 className={'sub-title text-2xl text-center'}>BHP - BHP Group Limited</h1>
+                <h1 className={'sub-title'}>BHP - BHP Group Limited</h1>
               </div>
             <div className={'col-span-12 md:col-span-6 border p-3'}>
               {/* Box 5 */}
@@ -28,32 +34,42 @@ function App() {
                   
                     <button className={'btn bg-red-600 hover:bg-red-500 m-2 rounded'}>Sell</button>
                 </div>
-              </div>
-                <div className={'col-span-12 border p-3'}>Box 6</div>
+            </div>
+            {/* Box 6 */}
+                <div className={'col-span-12 border p-3'}></div>
               </div>
             </div>
         
     
 
-            <div className={'col-span-12 md:col-span-6 border p-5'}>
-            <h1 className={'text-2xl text-center font-bold'}>Box 2</h1>
-            <table className={'border w-full'}>
-              <tr className={'border'}>
-                <th>Company</th>
-                <th>Contact</th>
-                <th>Country</th>
+        <div className={'col-span-12 md:col-span-6 border p-5'}>
+          {/* Box 2 */}
+            <h1 className={'sub-title'}>Portfolio</h1>
+            <table className={'border w-full m-2'}>
+              <tr className={'border sub-title2 bg-gray-100'}>
+                <th>Stock</th>
+                <th>Quantity</th>
+                <th>Current Value</th>
+                <th>Buy/Sell</th>
               </tr>
-              <tr className={'border'}>
-                <td>Alfreds Futterkiste</td>
-                <td>Maria Anders</td>
-                <td>Germany</td>
+              <tr className={'border p-text'}>
+                <td>BHP</td>
+                <td> </td>
+                <td> </td>
               </tr>
-              <tr className={'border'}>
-                <td>Centro comercial Moctezuma</td>
-                <td>Francisco Chang</td>
-                <td>Mexico</td>
+              <tr className={'border p-text'}>
+                <td>NAB</td>
+                <td> </td>
+                <td> </td>
+            </tr>
+            <tr className={'border p-text'}>
+                <td>Cash</td>
+                <td> </td>
+                <td> </td>
               </tr>
-            </table>         
+          </table>   
+          
+          
             </div>
       </div>
       
